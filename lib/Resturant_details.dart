cb52c5a0ee1d8d718357a3487b44de8590e1f85a
moura <muradhani>
2023-04-22 17:12:58 +0200

    display tables as a gray cards

--- lib/Resturant_details.dart
+++ lib/Resturant_details.dart
@@ -65,9 +65,55 @@ class RestaurantDetailsPage extends StatelessWidget {
                 style: TextStyle(fontSize: 16.0),
               ),
             ),
+            TableLayout(restaurant: restaurant),
           ],
         ),
       ),
+    );
+  }
+}
+
+class TableLayout extends StatelessWidget {
+  final Restaurant restaurant;
+
+  const TableLayout({Key? key, required this.restaurant}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: restaurant.numTables,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey[300],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Table ${index + 1}',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${restaurant.numSeats} seats',
+                style: TextStyle(fontSize: 14),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
